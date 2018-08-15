dcccd730a1cc797930e739252a74020b03f56a23
oliver-zb <42172575+oliver-zb@users.noreply.github.com>
2018-08-15 13:29:06 +0200

    edited MilkyStream_dict.py 15.08.2018
    positions and velocities are now correct
    useless lines deleted

--- MilkyStream_dict.py
+++ MilkyStream_dict.py
@@ -44,9 +44,7 @@ start = timeit.default_timer()
 # #    print('len(dict) without empty cells', len(dict))
 #     return dict
 #
-# #data = preview_csv("data/GaiaSource_2851858288640_1584379458008952960.csv", ',') # approx. 41 seconds
 # data = preview_csv("data/GaiaSource_6714230465835878784_6917528443525529728.csv", ';') # approx 8 seconds
-# # #print('type(data):', type(data))
 # data.update(preview_csv("data/GaiaSource_2851858288640_1584379458008952960.csv", ','))
 # data.update(preview_csv("data/GaiaSource_1584380076484244352_2200921635402776448.csv", ','))
 # data.update(preview_csv("data/GaiaSource_2200921875920933120_3650804325670415744.csv", ','))
@@ -60,9 +58,13 @@ start = timeit.default_timer()
 #     if eval(data[key][4]) <= 0.001:
 #         delete.append(key)
 #         "to delete stars with too small parallax (data too bad)"
+#     elif eval(data[key][5])/eval(data[key][4]) > 0.1:
+#         delete.append(key)
+#         "delete parallaxes with error more than 10% because positionns far away with big error" \
+#         "have bad influence on result"
+#
 # for i in delete:
 #     data.pop(i, None)
-# #print('len(data) without too small parallaxes:', len(data))
 #
 # key = list(data.keys())
 # ra = []
@@ -71,45 +73,32 @@ start = timeit.default_timer()
 # pmra = []
 # pmdec = []
 # rv = []
+# rv_err = []
+# par_err = []
+# pmra_err = []
+# pmdec_err = []
+# ra_err = []
+# dec_err = []
 #
 # for key in data:
 #     ra.append(eval(data[key][0]))
+#     ra_err.append(eval(data[key][1]))
 #     dec.append(eval(data[key][2]))
+#     dec_err.append(eval(data[key][3]))
 #     parallax.append(eval(data[key][4]))
 #     pmra.append(eval(data[key][7]))
 #     pmdec.append(eval(data[key][9]))
 #     rv.append(eval(data[key][21]))
+#     rv_err.append(eval(data[key][22]))
+#     par_err.append(eval(data[key][5]))
+#     pmra_err.append(eval(data[key][8]))
+#     pmdec_err.append(eval(data[key][10]))
 #
+# par_err=np.array([i for i in par_err])
 #
-# #ra = [float(i) for i in ra]
-# #dec = [float(i) for i in dec]
-# #parallax = [float(i) for i in parallax]
-# dist = [1 / i for i in parallax]  # / 1000 because of milliarcsec, dimension: parsec -> velocities make sense, positions not
-# #pmra = [float(i) for i in pmra]
-# #pmdec = [float(i) for i in pmdec]
-# #rv = [float(i) for i in rv]
+# dist = np.array([1000 / i for i in parallax])  # * 1000 because of milliarcsec, dimension: parsec -> velocities make sense, positions not
 #
-# # print('minimal parallax', min(parallax))
-#
-# #print('ra[0] = ',ra[0], 'type ra[0]', type(ra[0]), 'len(ra) = ', len(ra))
-#
-# "normalizes components such that mean of each value is zero => subtract mean of every variable"
-# #ra_m = sum(ra)/len(ra)
-# #print('ra_m =', ra_m)
-# #ra_new = [i - ra_m for i in ra]
-# #print('ra_new =', ra_new)
-# # dec_m = sum(dec)/len(dec)
-# # dec_new = [i - dec_m for i in dec]
-# # parallax_m = sum(parallax)/len(parallax)
-# # dist = [1 / i for i in parallax]
-# # parallax_new = [i - parallax_m for i in parallax]
-# # pmra_m = sum(pmdec)/len(pmra)
-# # pmra_new = [i - pmra_m for i in pmra]
-# # pmdec_m = sum(pmdec)/len(pmdec)
-# # pmdec_new = [i - pmdec_m for i in pmdec]
-# # rv_m = sum(rv)/len(rv)
-# # rv_new = [i - rv_m for i in rv]
-#
+# "correct method for positions"
 # x = np.array(dist * np.cos(dec) * np.cos(ra))
 # y = np.array(dist * np.cos(dec) * np.sin(ra))
 # z = np.array(dist * np.sin(dec))
@@ -117,80 +106,71 @@ start = timeit.default_timer()
 # q = np.array([x,y,z])
 #
 #
-# #print('q[:5]',q[:4])
+# "transverse velocities in ra, dec, pmx = mu_x(arcseconds/year) * distance * 4.740 " \
+# "-> * 1/ 1000 because we have mas" \
+# "4.740 to convert as/year*dist in parsec to km/sec to have same dimension as rv"
 #
-# "4.740 to convert mas/year*dist in parsec to km/sec to have same dimension as rv"
-# # v_x = dist * (rv * np.cos(dec) * 4.740 * (np.cos(ra) - np.sin(ra) * pmra - np.cos(ra) * np.sin(dec) * pmdec))
-# # v_y = dist * (rv * np.cos(dec) * 4.740 * (np.sin(ra) + np.cos(ra) * pmra - np.sin(ra) * np.sin(dec) * pmdec))
-# # v_z = dist * (rv * np.sin(dec) + 4.740 * np.cos(dec) * pmdec)
-#
-"transverse velocities in ra, dec, pmx = mu_x(arcseconds/year) * distance * 4.740"
-"when d not /1000 for position but for velocity (0.0047) -> position (everything within 1000 pc) and velocity make sense but this must be wrong"
-"when d/1000 for position and velocity (4.47) -> positions dont make sense (everything within 1 pc)"
-# v_tra = np.array(pmra) * np.array(dist) * 0.00474 # x_2 with 4.74 -> positions make sense but this method must be wrong, x with 0.00474 -> positions dont ma
-# v_tdec = np.array(pmdec) * np.array(dist) * 0.00474
+# v_tra = 1 / 1000 * np.array(pmra) * np.array(dist) * 4.74 # * 1000 because of mas
+# v_tdec = 1 / 1000 * np.array(pmdec) * np.array(dist) * 4.74
 # "cartesian velocities, 1/977780 to convert km/sec to pc/year"
 # v_x = (rv * np.cos(dec) * np.cos(ra) - v_tra * np.sin(ra) - v_tdec * np.sin(dec) * np.cos(ra)) / 977780
 # v_y = (rv * np.cos(dec) * np.sin(ra) + v_tra * np.cos(ra) - v_tdec * np.sin(dec) * np.cos(ra)) / 977780
 # v_z = (rv * np.sin(dec) + v_tdec * np.cos(dec)) / 977780
 # v = np.array([v_x,v_y,v_z])
 
-# "saves cartesian coordinates and velocities"
+# "saves values, reading those is much faster than doing all the things above every time"
+# # np.save('x_final', x)
+# # np.save('y_final', y)
+# # np.save('z_final', z)
+# # np.save('q_final', q)
+# #
+# # v_x = np.array(v_x)
+# # v_y = np.array(v_y)
+# # v_z = np.array(v_z)
+# #
+# # np.save('v_x_final', v_x)
+# # np.save('v_y_final', v_y)
+# # np.save('v_z_final', v_z)
+# # np.save('v_final1', v)
+# #
+# # np.save('par_err', par_err)
+# # np.save('parallax', parallax)
+# # np.save('pmra', pmra)
+# # np.save('pmra_err',pmra_err)
+# # np.save('pmdec', pmdec)
+# # np.save('pmdec_err', pmdec_err)
+# # np.save('ra', ra)
+# # np.save('dec', dec)
+# # np.save('rv', rv)
+# # np.save('rv_err', rv_err)
+# # np.save('ra_err',ra_err)
+# # np.save('dec_err', dec_err)
 
-"2: d/1000, 4.47    3: d*1, 4.47,    4 d*1, 0.0047"
-np.save('x_4', x)
-np.save('y_4', y)
-np.save('z_4', z)
-np.save('q_4', q)
+" -> loading those is much faster than compiling dict every time"
 
-v_x = np.array(v_x)
-v_y = np.array(v_y)
-v_z = np.array(v_z)
+x = np.load('x_final.npy')
+y = np.load('y_final.npy')
+z = np.load('z_final.npy')
+q = np.load('q_final.npy')
+v_x = np.load('v_x_final.npy')
+v_y = np.load('v_y_final.npy')
+v_z = np.load('v_z_final.npy')
+v = np.load('v_final.npy')
 
-np.save('v_x_4', v_x)
-np.save('v_y_4', v_y)
-np.save('v_z_4', v_z)
-np.save('v_4', v)
+# np.load('par_err.npy')
+# np.load('parallax.npy')
+# np.load('pmra.npy')
+# np.load('pmra_err.npy')
+# np.load('pmdec.npy')
+# np.load('pmdec_err.npy')
+# np.load('ra.npy')
+# np.load('dec.npy')
+# np.load('rv.npy')
+# np.load('rv_err.npy')
+# np.load('ra_err.npy')
+# np.load('dec_err.npy')
 
-" -> loading those is much faster than comiling dict every time"
-
-# x = np.load('x.npy')
-# y = np.load('y.npy')
-# z = np.load('z.npy')
-# q = np.load('q.npy')
-# v_x = np.load('v_x.npy')
-# v_y = np.load('v_y.npy')
-# v_z = np.load('v_z.npy')
-# v = np.load('v.npy')
-
-# x = np.load('x_2.npy')
-# y = np.load('y_2.npy')
-# z = np.load('z_2.npy')
-# q = np.load('q_2.npy')
-# v_x = np.load('v_x_2.npy')
-# v_y = np.load('v_y_2.npy')
-# v_z = np.load('v_z_2.npy')
-# v = np.load('v_2.npy')
-
-# x = np.load('x_3.npy')
-# y = np.load('y_3.npy')
-# z = np.load('z_3.npy')
-# q = np.load('q_3.npy')
-# v_x = np.load('v_x_3.npy')
-# v_y = np.load('v_y_3.npy')
-# v_z = np.load('v_z_3.npy')
-# v = np.load('v_3.npy')
-
-x = np.load('x_4.npy')
-y = np.load('y_4.npy')
-z = np.load('z_4.npy')
-q = np.load('q_4.npy')
-v_x = np.load('v_x_4.npy')
-v_y = np.load('v_y_4.npy')
-v_z = np.load('v_z_4.npy')
-v = np.load('v_4.npy')
-
-"prints velocity distribution in cartesian coordinates"
+"velocity distribution in cartesian coordinates"
 
 # fig = plt.figure()
 # ax = Axes3D(fig)
@@ -199,10 +179,10 @@ v = np.load('v_4.npy')
 # ax.set_xlabel("x [pc/year]")
 # ax.set_ylabel("y [pc/year]")
 # ax.set_zlabel('z [pc/year]')
-# # plt.xlim(-0.075,0.075)
-# # plt.ylim(-0.075,0.075)
-# # ax.set_zlim(-0.075,0.075)
-# plt.savefig('vel_distr_all_d=1_0.004740_scaled_dist.png', format='png', dpi=1200)
+# # plt.xlim(-50,50)
+# # plt.ylim(-50,50)
+# # ax.set_zlim(-50,50)
+# plt.savefig('vel_distr_final.png', format='png', dpi=1200)
 #
 # plt.show()
 
@@ -215,7 +195,7 @@ v = np.load('v_4.npy')
 # ax.set_ylabel("y [pc]")
 # ax.set_zlabel('z [pc]')
 # ax.set_title('positions in cartesian coordinates')
-# plt.savefig('pos_distr_all_d=1_0.004740_scaled_dist.png', format='png', dpi=1200)
+# plt.savefig('pos_distr_final.png', format='png', dpi=1200)
 #
 # plt.show()
 
@@ -271,18 +251,18 @@ v = np.load('v_4.npy')
 # ax = Axes3D(fig)
 #
 #
-# points, = ax.plot(x, y, z, linestyle='', marker='.',markersize=2)
+# points, = ax.plot(x, y, z, linestyle='', marker='.',markersize=1.75)
 # txt = fig.suptitle('')
 #
 # def update_points(t, x, y, z, points):
-#     txt.set_text('{:d} * 100 years'.format(t)) # for debug purposes
+#     txt.set_text('{:d} * 1000 years'.format(t)) # for debug purposes
 #      #"calculate the new sets of coordinates here. The resulting arrays should have the same shape " \
 #      #"as the original x,y,z"
 #
 #
-#     new_x = x[: ]+v_x[: ]*t*100
-#     new_y = y[: ]+v_y[: ]*t*100
-#     new_z = z[: ]+v_z[: ]*t*100
+#     new_x = x[: ]+v_x[: ]*t*1000
+#     new_y = y[: ]+v_y[: ]*t*1000
+#     new_z = z[: ]+v_z[: ]*t*1000
 #     #print('t:', t)
 #     # update properties
 #     points.set_data(new_x,new_y)
@@ -343,46 +323,68 @@ v = np.load('v_4.npy')
 # fig = plt.figure()
 # ax = plt.axes()
 # txt = fig.suptitle('')
-# frames = 2000
+# frames = 1000
 #
 #
 # z_c = 0  # let cameras be placed on x-axis
-# x_c = 10  # assume first only one camera in origin, later two cameras separated by distance of the eyes (65mm)
+# #y_c = 0
+# x_c = 10   # assume first only one camera in origin, later two cameras separated by distance of the eyes (65mm)
 #
 # "könnte noch faktor  3.086e+2 (von 3.086e+16 für pc -> m) mit position und geschwindigkeit multiplizieren)"
 # "dann aber abstand zwischen rot und blau kleiner"
 #
-# x_s = np.array(q[0])
-# y_s = np.array(q[1])
-# z_s = np.array(q[2])
-# v_x = np.array(v[0])
-# v_y = np.array(v[1])
-# v_z = np.array(v[2])
+# x_s = np.array(q[0][:20000])
+# y_s = np.array(q[1][:20000])
+# z_s = np.array(q[2][:20000])
+# v_x = np.array(v[0][:20000])
+# v_y = np.array(v[1][:20000])
+# v_z = np.array(v[2][:20000])
 #
 # y_c = 0.5
+# # z_c = 0.5
 #
 # y_min = min(q[1])
 # y_sc = y_min + y_s
-# "faktor 10 vergrössert abstand zwischen rot und blau"
-# lambd = (y_c / (y_c - y_sc))*10
+# # z_min = min(q[2])
+# # z_sc = z_min + z_s
+#
+# "zusätzlicher faktor zu lambda vergrössert abstand zwischen rot und blau"
+# "jeweils erster teil: y-achse 3dim, zweiter teil: z-achse 3dim"
+# lambd = (y_c / (y_c - y_sc))
 # x_sc = lambd * (x_s - x_c) + x_c
 # z_sc = lambd * (z_s - z_c) + z_c
 # x_sc_1 = x_sc + lambd / 2
 # x_sc_2 = x_sc - lambd / 2
 # z_sc_1 = z_sc + lambd / 2
 # z_sc_2 = z_sc - lambd / 2
+# # lambd = (z_c / (z_c - z_sc))
+# # x_sc = lambd * (x_s - x_c) + x_c
+# # y_sc = lambd * (y_s - y_c) + y_c
+# # x_sc_1 = x_sc + lambd / 2
+# # x_sc_2 = x_sc - lambd / 2
+# # y_sc_1 = y_sc + lambd / 2
+# # y_sc_2 = y_sc - lambd / 2
 #
 # y_new = []
+# #z_new = []
 # lambd = []
 # for t in range(0,frames,1):
 #     y_min = min(y_s + v_y * t * 100)
 #     y_sc = y_min + y_s * v_y * t * 100
-#     lambd_new = y_c / (y_c - y_sc) * 10
+#     lambd_new = y_c / (y_c - y_sc)
 #     y_new.append(y_sc)
 #     lambd.append(lambd_new)
+# # for t in range(0,frames,1):
+# #     z_min = min(z_s + v_z * t * 100)
+# #     z_sc = z_min + z_s * v_z * t * 100
+# #     lambd_new = z_c / (z_c - z_sc)
+# #     z_new.append(z_sc)
+# #     lambd.append(lambd_new)
 #
-# line_1, = ax.plot(x_sc_1, z_sc_1,c='r',linestyle='', marker='.',markersize=2, alpha=0.6)
-# line_2, = ax.plot(x_sc_2, z_sc_2, c='c',linestyle='', marker='.',markersize=2, alpha=0.6)
+# line_1, = ax.plot(x_sc_1, z_sc_1,c='r',linestyle='', marker='.',markersize=5, alpha=0.8)
+# line_2, = ax.plot(x_sc_2, z_sc_2, c='c',linestyle='', marker='.',markersize=5, alpha=0.4)
+# # line_1, = ax.plot(x_sc_1, y_sc_1,c='r',linestyle='', marker='.',markersize=5, alpha=0.8)
+# # line_2, = ax.plot(x_sc_2, y_sc_2, c='c',linestyle='', marker='.',markersize=5, alpha=0.4)
 #
 # # initialization function: plot the background of each frame
 #
@@ -391,7 +393,7 @@ v = np.load('v_4.npy')
 #     line_2.set_data([], [])
 #     return line_1, line_2,
 #
-# # animation function.  This is called sequentially
+# "animation function.  This is called sequentially"
 # def animate(i):
 #     txt.set_text('{:d} * 100 years'.format(i))  # for debug purposes
 #     new_x_1 = x_sc_1[:] + v_x[:] * i * 100
@@ -407,17 +409,32 @@ v = np.load('v_4.npy')
 #     line_2.set_data(new_x_2, new_z_2)
 #     return line_1, line_2,
 #
+# # def animate(i):
+# #     txt.set_text('{:d} * 100 years'.format(i))  # for debug purposes
+# #     new_x_1 = x_sc_1[:] + v_x[:] * i * 100
+# #     new_y_1 = y_sc_1[:] + v_y[:] * i * 100
+# #     # new_x_1 = x_sc_1 + v_x * i * 100
+# #     # new_z_1 = z_sc + v_z * i * 100
+# #
+# #     # new_x_2 = x_sc_2[:10000] + v_x[:10000] * i * 100
+# #     # new_z_2 = z[:10000] + v_z[:10000] * i * 100
+# #     new_x_2 = x_sc_2[:] + v_x[:] * i * 100
+# #     new_y_2 = y_sc_2[:] + v_y[:] * i * 100
+# #     line_1.set_data(new_x_1, new_y_1)
+# #     line_2.set_data(new_x_2, new_y_2)
+# #     return line_1, line_2,
+#
+#
 # # call the animator.  blit=True means only re-draw the parts that have changed.
 # anim = animation.FuncAnimation(fig, animate,
-#                                frames=frames, interval=100, blit=False, init_func=init)
+#                                frames=frames, interval=50, blit=False, init_func=init)
 #
-# plt.xlim(-0,40)
+# plt.xlim(-10,40)
 # plt.ylim(-20,20)
 #
 # plt.show()
 
-#print('considered data points :',len(data))  # final number of data points that was used
+print('considered data points :',len(x))  # final number of data points that was used
 
 stop = timeit.default_timer()
 print("calculation time:", round(stop - start),"s")
-
