a0ac32f8e56d725736689fb879ba5f97c0e48b52
Prasenjit Saha <psaha@physik.uzh.ch>
2018-09-07 07:33:20 +0200

    starting to read

---
+++ vels/reader.py
@@ -0,0 +1,54 @@
+fil = open('GaiaSource_1584380076484244352_2200921635402776448.csv')
+head = fil.readline().split(',')
+
+#for i,x in enumerate(head):
+#    print(i,x)
+
+dic = { 'ra': 5, 'dec': 7, 'parallax': 9,
+        'pmra': 12, 'pmdec': 14, 'radial_velocity': 66,
+        'teff_val': 78
+      }
+
+cols = (dic['ra'],dic['ra']+1,
+        dic['dec'],dic['dec']+1,
+        dic['parallax'],dic['parallax']+1,
+        dic['pmra'],dic['pmra']+1,
+        dic['pmdec'],dic['pmdec']+1,
+        dic['radial_velocity'],dic['radial_velocity']+1,
+        dic['teff_val'],dic['teff_val']+1,dic['teff_val']+2)
+
+print(cols)
+for i in cols:
+    print(head[i])
+
+for s in range(5):
+    star = fil.readline().split(',')
+    for i in (5,7,9,12,14,66,78):
+        print(head[i],end=' ')
+    print()
+    for i in (5,7,9,12,14,66,78):
+        print(star[i],end=' ')
+    print()
+
+fil.close()
+
+import numpy
+
+fname = 'GaiaSource_1584380076484244352_2200921635402776448.csv'
+
+lots = numpy.genfromtxt(fname, delimiter=',', skip_header=1,
+       filling_values=-999, usecols=cols)
+
+parallax_ok = 10*lots[:,5] < lots[:,4]
+pmra_ok = 10*lots[:,7] < abs(lots[:,6])
+pmdec_ok = 10*lots[:,9] < abs(lots[:,8])
+rv_ok = 10*lots[:,11] < abs(lots[:,10])
+teff_ok = 5*(lots[:,14]-lots[:,13]) < lots[:,12]
+
+lots = lots[parallax_ok*pmra_ok*pmdec_ok*rv_ok*teff_ok,:]
+
+for s in range(1):
+    print(lots[s,0],lots[s,2],lots[s,4],lots[s,6],lots[s,8],lots[s,10],
+          lots[s,12])
+print(lots.shape)
+
---
+++ vels/rotmatr.py
@@ -0,0 +1,27 @@
+from numpy import matrix, cos, sin, pi
+
+alphaG = 192.85948 * pi/180
+deltaG = 27.12825 * pi/180
+lOm = 32.93192 * pi/180
+
+x = -lOm
+A = matrix([ [ cos(x), -sin(x), 0 ],
+             [ sin(x),  cos(x), 0 ],
+             [        0,     0, 1 ] ])
+
+x = pi/2 - deltaG
+B = matrix([ [ 1,       0,       0 ],
+             [ 0,  cos(x), -sin(x) ],
+             [ 0,  sin(x),  cos(x) ] ])
+
+
+x = alphaG + pi/2
+C = matrix([ [ cos(x), -sin(x), 0 ],
+             [ sin(x),  cos(x), 0 ],
+             [      0,       0, 1 ] ])
+
+M = matrix([ [  1,  0,  0 ],
+             [  0, -1,  0 ],
+             [  0,  0,  1 ] ])
+
+print(M*A*B*C*M)
